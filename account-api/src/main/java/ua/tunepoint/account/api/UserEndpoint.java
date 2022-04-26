d86094e4259679278d1a7041ce0498c5e214be49
Kovalev Danil <opana.loog@gmail.com>
2022-04-27 01:46:15 +0300

    fix api

--- account-api/src/main/java/ua/tunepoint/account/api/UserEndpoint.java
+++ account-api/src/main/java/ua/tunepoint/account/api/UserEndpoint.java
@@ -3,10 +3,10 @@ package ua.tunepoint.account.api;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import ua.tunepoint.account.model.response.payload.UserPublicPayload;
+import ua.tunepoint.account.model.response.UserPublicResponse;
 
 public interface UserEndpoint {
 
     @GetMapping("/accounts/users/{id}")
-    ResponseEntity<UserPublicPayload> findUser(@PathVariable Long id);
+    ResponseEntity<UserPublicResponse> findUser(@PathVariable Long id);
 }
