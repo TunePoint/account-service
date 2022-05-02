94823085e2d7d2747df97e7b6a334b6b53620bbf
Kovalev Danil <opana.loog@gmail.com>
2022-05-03 01:41:45 +0300

    update model

--- account-model/src/main/java/ua/tunepoint/account/model/response/domain/Statistics.java
+++ account-model/src/main/java/ua/tunepoint/account/model/response/domain/Statistics.java
@@ -2,9 +2,11 @@ package ua.tunepoint.account.model.response.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Statistics {
 
     private Long followerCount;
